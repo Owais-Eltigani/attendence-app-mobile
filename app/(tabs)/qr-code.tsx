3411ed08ec4967c45ae049bc633c5b31d888a143
Dexter <dexter@Dexters-MacBook-Air.local>
2025-10-13 20:33:50 +0530

    #3 working on the camera module

--- app/(tabs)/qr-code.tsx
+++ app/(tabs)/qr-code.tsx
@@ -1,13 +1,93 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useEffect, useState } from 'react';
+// import { SafeAreaView } from 'react-native-safe-area-context'; //? enabled if run to issues with aligning in android.
+import { CameraView, Camera } from 'expo-camera';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 const Qrcode = () => {
+  const [hasPermission, setHasPermission] = useState(false); //? set to null if faced any issues
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const handleBarcodeScanned = ({ type, data }) => {
+    setScanned(true);
+    // alert(`Bar code with  data ${data} has been scanned!`);
+    console.log(data);
+  };
+
+  //* add area view if needed.
   return (
-    <SafeAreaView>
-      <Text> Code Screen</Text>
-    </SafeAreaView>
+    <View className="flex-1 ">
+      <StatusBar hidden={true} translucent={true} />
+      {/* scanner window */}
+      <View className="mb-6 h-[550px] items-center justify-center overflow-hidden rounded-lg bg-gray-200 shadow-lg">
+        {/* <CameraView style={styles.camera} facing={'back'}></CameraView>  */}
+
+        <CameraView
+          onBarcodeScanned={scanned ? undefined : handleBarcodeScanned}
+          barcodeScannerSettings={{
+            barcodeTypes: ['qr'],
+            isGuidanceEnabled: true, // Shows guidance overlays on iOS
+          }}
+          style={StyleSheet.absoluteFillObject}
+        />
+        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      </View>
+
+      {/* mode switcher */}
+      <View className="h-32 w-32 items-center justify-center self-center rounded-full bg-blue-300 p-3">
+        <Text className="text-center font-extrabold text-white">Mode Switcher</Text>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+    // minHeight: 300, // Ensure minimum height for camera
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
 
 export default Qrcode;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
     <SafeAreaProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)/qr-code" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </SafeAreaProvider>
